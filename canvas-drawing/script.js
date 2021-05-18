1902a07c5706ee0a9d1984c79776d08bfcdf5a6c
kr-nkit <ankitkr0696@gmail.com>
2021-05-18 17:22:13 +0530

    - ve fix

--- canvas-drawing/script.js
+++ canvas-drawing/script.js
@@ -70,7 +70,7 @@ increaseBtn.addEventListener('click', () => {
 
 decreaseBtn.addEventListener('click', () => {
   size -= 5
-  if (size > 50) {
+  if (size < 5) {
     size = 5
   }
   updateSizeOnScreen()
