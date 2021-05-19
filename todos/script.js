91005d1e18b5374ce9f79110bc810382725f6fb9
kr-nkit <ankitkr0696@gmail.com>
2021-05-19 17:45:26 +0530

    local storage added

--- todos/script.js
+++ todos/script.js
@@ -2,6 +2,12 @@ const from = document.getElementById('form')
 const input = document.getElementById('input')
 const todosUL = document.getElementById('todos')
 
+const todos = JSON.parse(localStorage.getItem('todos'))
+if (todos) {
+  todos.forEach((todo) => {
+    addTodo(todo)
+  })
+}
 from.addEventListener('submit', (e) => {
   e.preventDefault()
   addTodo()
@@ -18,18 +24,35 @@ function addTodo(todo) {
     if (todo && todo.completed) {
       todoEl.classList.add('completed')
     }
-
+    //remove todo
     todoEl.innerHTML = '<i class="far fa-times-circle delete"></i>' + todoText
     let tickEl = todoEl.getElementsByTagName('i')
     tickEl[0].addEventListener('click', (e) => {
       todoEl.remove()
+      updateLS()
     })
+    //strike out todo
     todoEl.addEventListener('click', (e) => {
       todoEl.classList.toggle('completed')
+      updateLS()
     })
 
     todosUL.appendChild(todoEl)
 
     input.value = ''
+    updateLS()
   }
+}
+
+function updateLS() {
+  todosEL = document.querySelectorAll('li')
+  const todos = []
+  todosEL.forEach((todoEL) => {
+    todos.push({
+      text: todoEL.innerText,
+      completed: todoEL.classList.contains('completed'),
+    })
+  })
+  console.log(todos)
+  localStorage.setItem('todos', JSON.stringify(todos))
 }
